313b9b30b3aa6c3ebf16ed955782136b54e39e48
surasak1129 <icenamang@gmail.com>
2017-02-09 18:05:09 +0700

    Hello git

---
+++ src/main/java/rmuti/lab4/ArrayListExt.java
@@ -0,0 +1,21 @@
+package rmuti.lab4;
+public class ArrayListExt extends ArraysList {
+
+	public static void main(String[] args){
+		ArrayListExt count = new ArrayListExt();
+		int sum = count.countItem(6);
+		System.out.println(sum);
+	}
+		
+	public int countItem(Object e) {
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (elementData[i].equals(e)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+
+}
